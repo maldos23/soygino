357b4482756ea92ebf608c1d7ca5e8f33c226696
ginoRX <ginor@seif.io>
2021-08-26 00:04:22 -0500

    Agrego description

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,30 @@ import { Fragment } from "react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Fragment>
-      <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Head>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</Fragment>
+	);
 }
 
 export default MyApp;
